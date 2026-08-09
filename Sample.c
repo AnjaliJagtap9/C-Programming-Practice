f8fbe0e433e2f36b0c6f05d1ea86bcdca90d2799
AnjaliJagtap9 <jagtapanjali246@gmail.com>
2026-08-09 22:16:43 +0530

    Implement number printing program in Sample.c
    Add a program to print numbers from 0 to n

---
+++ Sample.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+
+   {
+   int n;
+   printf("enter a no: ");
+   scanf("%d",&n);
+   int i=0;
+   while(i<=n){
+   printf( " %d \n",i);
+   i++;
+   }
+    return 0;
+   }
