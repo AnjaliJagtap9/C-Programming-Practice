b7e8c94936ac9f39cdce1d6deba35624601a36ab
AnjaliJagtap9 <jagtapanjali246@gmail.com>
2026-08-09 22:31:45 +0530

    Add do-while loop to print numbers up to n

---
+++ Numberusingdowhile.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+
+   {
+   int n;
+   printf("enter a no: ");
+   scanf("%d",&n);
+   int i=0;
+   do{
+   printf( " %d \n",i);
+   i++;
+   }
+   while(i<=n);
+   
+    return 0;
+   }
