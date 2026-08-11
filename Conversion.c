b99a7ba9f629b81ad45c4493fce6a7ae15308e1e
AnjaliJagtap9 <jagtapanjali246@gmail.com>
2026-08-11 19:23:45 +0530

    Implement basic addition program in C
    This program takes two integers as input and outputs their sum as a float.

---
+++ Conversion.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<stdbool.h>
+
+int main()
+{
+int n,m;
+  printf("enter.a no: ");
+  scanf("%d",&n);
+   printf("enter m=");
+   scanf("%d",&m);
+
+
+float result = (float)n+m;
+
+printf("%f", result);
+   printf("Thank you!!");
+    return 0;
+}
