d6e1950caec62e9815c78b8756f0c63012bcca90
AnjaliJagtap9 <jagtapanjali246@gmail.com>
2026-08-11 18:01:36 +0530

    Add Printsizeof.c to display data type sizes
    Prints the sizes of various data types in C.

---
+++ Printsizeof.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+
+   printf("The size of int: %d\n ",sizeof(int));
+   printf("The size of float : %zu\n",sizeof(float));
+   printf("The size of float : %zu\n",sizeof(double));
+   printf("The size of float : %zu\n",sizeof(char));
+    return 0;
+}
