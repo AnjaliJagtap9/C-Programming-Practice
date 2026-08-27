9bba14e0a44eebcf725dfd68afc1e6144f7f0f26
AnjaliJagtap9 <jagtapanjali246@gmail.com>
2026-08-27 17:42:46 +0530

    Implement palindrome check function in Leetvode9.c

---
+++ Leetvode9.c
@@ -0,0 +1,19 @@
+#include <stdbool.h>
+
+bool isPalindrome(int x) {
+    
+    if (x < 0)
+        return false;
+    
+    if (x != 0 && x % 10 == 0)
+        return false;
+
+    int reverse = 0;
+
+    while (x > reverse) {
+        reverse = reverse * 10 + x % 10;
+        x = x / 10;
+    }
+
+    return x == reverse || x == reverse / 10;
+}
