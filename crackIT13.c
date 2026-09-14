5b2c00ab715967dc9786f13fef52b774990724dd
AnjaliJagtap9 <jagtapanjali246@gmail.com>
2026-09-14 23:55:57 +0530

    Implement square and derived value calculation
    Calculate square and derived value from user input.

---
+++ crackIT13.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+int n;
+printf("enter a number:");
+scanf("%d",&n);
+int square=n*n;
+int frd=n*n+2*n;
+printf("square = %d\n",square);
+printf("the value are: %d\n",frd);
+
+    return 0;
+}
