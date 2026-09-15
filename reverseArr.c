a08c020f3073fb1da5705388956335bcc0bfca45
Anjali Jagtap <jagtapanjali246@gmail.com>
2026-09-16 01:16:58 +0530

    add reverseArr array reverse program

---
+++ reverseArr.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+int size;
+printf("enter size of array:");
+scanf("%d",&size);
+int arr[size];
+printf("enter elements in an array :\n");
+for(int i=0;i<size;i++){
+    scanf("%d",&arr[i]);
+}
+printf("reverse array :");
+for(int i=size-1;i>=0;i--){
+printf("%d",arr[i]);
+}
+    return 0;
+}
