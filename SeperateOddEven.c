4f4099e8733651ba557366242f7953c23ca90b42
AnjaliJagtap9 <jagtapanjali246@gmail.com>
2026-09-26 21:47:04 +0530

    Implement program to separate odd and even numbers

---
+++ SeperateOddEven.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+
+    printf("Enter array size: ");
+    scanf("%d", &n);
+
+    int arr[n];
+
+    printf("Enter array elements:\n");
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    printf("Even elements: ");
+    for (int i = 0; i < n; i++) {
+        if (arr[i] % 2 == 0) {
+            printf("%d ", arr[i]);
+        }
+    }
+
+    printf("\nOdd elements: ");
+    for (int i = 0; i < n; i++) {
+        if (arr[i] % 2 != 0) {
+            printf("%d ", arr[i]);
+        }
+    }
+
+    return 0;
+}
